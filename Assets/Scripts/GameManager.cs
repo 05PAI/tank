ee30bf787c4bb094575e3fd3b055ceee41da6d78
05_PAI <s24511746@gmail.com>
2021-12-16 13:59:53 +0800

    add spawnpoint

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,6 +2,7 @@ using Photon.Pun;
 using Photon.Realtime;
 using System.Collections;
 using System.Collections.Generic;
+using Tanks;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -10,9 +11,14 @@ public class GameManager : MonoBehaviourPunCallbacks
     public static GameManager instance;
     string gameVersion = "1";
     public static GameObject localPlayer;
+    private GameObject defaultSpawnPoint;
 
     void Awake()
     {
+        defaultSpawnPoint = new GameObject("Default SpawnPoint");
+        defaultSpawnPoint.transform.position= new Vector3(0, 0, 0);
+        defaultSpawnPoint.transform.SetParent(transform, false);
+
         if (instance != null)
         {
             Debug.LogErrorFormat(gameObject,
@@ -69,8 +75,31 @@ public class GameManager : MonoBehaviourPunCallbacks
         {
             return;
         }
-        localPlayer = PhotonNetwork.Instantiate("TankPlayer", new Vector3(0, 0, 0), Quaternion.identity, 0);
+
+        var spawnPoint = GetRandomSpawnPoint();
+
+        localPlayer = PhotonNetwork.Instantiate("TankPlayer", spawnPoint.position, spawnPoint.rotation, 0);
         Debug.Log("Player Instance ID: " + localPlayer.GetInstanceID());
+    }
+    public static List<GameObject> GetAllObjectsOfTypeInScene<T>()
+    {
+        var objectsInScene = new List<GameObject>();
+        foreach (var go in (GameObject[])Resources.FindObjectsOfTypeAll(typeof(GameObject)))
+        {
+            if (go.hideFlags == HideFlags.NotEditable ||
+                go.hideFlags == HideFlags.HideAndDontSave)
+                continue;
+            if (go.GetComponent<T>() != null)
+                objectsInScene.Add(go);
+        }
+        return objectsInScene;
+    }
+    private Transform GetRandomSpawnPoint()
+    {
+        var spawnPoints = GetAllObjectsOfTypeInScene<SpawnPoint>();
+        return spawnPoints.Count == 0
+        ? defaultSpawnPoint.transform
+        : spawnPoints[Random.Range(0, spawnPoints.Count)].transform;
     }
 
 }
